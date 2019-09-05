d0f9c20cdb3ede43fa5348df0890049ca1d91859
caiouehara <caioueharamartins@gmail.com>
2019-09-04 22:59:52 -0300

    Create backend MongoDB

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import '../css/App.css';
 import ReferenceCoin from './EurExchange/ReferenceCoin';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>EUR EXCHANGE BASE</h2>
-        <ReferenceCoin></ReferenceCoin>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+
+      <h1>EUR Exchange Base</h1>
+      <ReferenceCoin></ReferenceCoin>
+    </div>
+  );
 }
 
 export default App;

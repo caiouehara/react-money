f5c4e14a8d8981b415963f67ae7419afc5567d91
caiouehara <caioueharamartins@gmail.com>
2019-09-05 20:49:16 -0300

    Create Nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/App.css';
+import Nav from './Nav/Nav';
 import EurExchange from './EurExchange/EurExchange';
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <EurExchange></EurExchange>
     </div>
   );
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../css/Nav/Nav.css'; 
+
+function Nav() {
+  return (
+    <div className="Nav">
+        <div className="menu">
+            <button>Home</button>
+            <button>Account</button>
+        </div>
+    </div>
+  );
+}
+
+export default Nav;

8acbe7ad792fa4d47f1dc77a20c9b1d2f73af9ef
caiouehara <caioueharamartins@gmail.com>
2019-09-05 18:06:53 -0300

    Refact EurExchange

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../css/App.css';
-import ReferenceCoin from './EurExchange/ReferenceCoin';
+import EurExchange from './EurExchange/EurExchange';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>EUR Exchange Base</h1>
-      <ReferenceCoin></ReferenceCoin>
+      <EurExchange></EurExchange>
     </div>
   );
 }
---
+++ src/components/EurExchange/EurExchange.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReferenceCoin from './ReferenceCoin';
+
+function EurExchange() {
+  return (
+    <div className="EurExchange">
+      <h1>EUR Exchange Base</h1>
+      <ReferenceCoin></ReferenceCoin>
+    </div>
+  );
+}
+
+export default EurExchange;

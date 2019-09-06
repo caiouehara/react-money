9f7fce820bcae91cb3bd503efa9ce999f42c22fa
caiouehara <caioueharamartins@gmail.com>
2019-09-05 21:46:19 -0300

    Refact Nav, Add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Nav from './Nav/Nav';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import EurExchange from './EurExchange/EurExchange';
 
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <EurExchange></EurExchange>
+      <Router>
+        <div className="nav">
+          <div className="nav-menu">
+            <Link to="/"><button>Home</button></Link>
+            <Link to="/eur-exchange"><button>Eur Exchange</button></Link>
+          </div>
+        </div>
+        
+        <Route path="/" exact/>
+        <Route path="/eur-exchange" component={EurExchange}/>
+      </Router>
+      <p>Home</p>
     </div>
   );
 }
